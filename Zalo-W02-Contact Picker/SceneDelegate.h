48229455abc6710683cff9f5d871c29428391c48
hieutran <trandinhtonhieu2@gmail.com>
2020-03-11 20:57:56 +0700

    Initial Commit

---
+++ Zalo-W02-Contact Picker/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Zalo-W02-Contact Picker
+//
+//  Created by Trần Đình Tôn Hiếu on 3/11/20.
+//  Copyright © 2020 Trần Đình Tôn Hiếu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Zalo-W02-Contact Picker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Zalo-W02-Contact Picker
+//
+//  Created by Trần Đình Tôn Hiếu on 3/11/20.
+//  Copyright © 2020 Trần Đình Tôn Hiếu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
